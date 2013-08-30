de037a7799fc3877826236fd3208adf75060c68e
ravichandran-dci <ravichandran.sithan@dci.in>
2013-08-31 00:36:21 +0530

    Provided comments to Report.xaml file

--- MeteredRateFareCalculation/MeteredRateFareCalculation/Report.xaml.cs
+++ MeteredRateFareCalculation/MeteredRateFareCalculation/Report.xaml.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/****************************************************************************************************
+ * Copyright © Dot Com Infoway. All rights reserved.      
+ * 
+ * Created By : Ravichandran
+ * Created Date : 30-Aug-2013
+ * Purpose : User Interface for showing calculated fare in detail.
+ * 
+ * Change History
+ * Updated By       Updated Date        Purpose/Changes made
+ * Ravichandran     30-Aug-2013         Initial Version
+ * ***************************************************************************************************/
+
+#region Assemblies
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +24,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using FareCalculationLibrary;
+#endregion
 
+#region Namespace
 namespace MeteredRateFareCalculation
 {
     /// <summary>
@@ -19,11 +34,20 @@ namespace MeteredRateFareCalculation
     /// </summary>
     public partial class Report : Window
     {
+        #region Constructors
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Report()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Initializes the form with the data provided
+        /// </summary>
+        /// <param name="objFareReport">Calculated fare.</param>
+        /// <param name="numberOfMilesTraveled">Number Of Miles Traveled.</param>
+        /// <param name="numberOfMinutesTraveled">Number Of Minutes Traveled</param>
         public Report(Fare objFareReport, Int32 numberOfMilesTraveled, Int32 numberOfMinutesTraveled)
         {
             InitializeComponent();
@@ -38,10 +62,19 @@ namespace MeteredRateFareCalculation
             lblNewYorkSurchargeValue.Text = String.Format(lblNewYorkSurchargeValue.Text, objFareReport.NewyorkStateTaxSurcharge);
             lblTotalFareValue.Text = String.Format(lblTotalFareValue.Text, objFareReport.MinimumFare + objFareReport.AdditionalFare + objFareReport.OneFifthOfMileFare + objFareReport.NightSurcharge + objFareReport.PeakHourSurcharge + objFareReport.NewyorkStateTaxSurcharge);
         }
+        #endregion
 
+        #region Event Handler
+        /// <summary>
+        /// Handles the close button click event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+        #endregion
     }
 }
+#endregion
