b362091d3569957023f192cc5bbf5f4e7e709e5e
ravichandran-dci <ravichandran.sithan@dci.in>
2013-08-30 22:03:55 +0530

    Recommit Code
    Since the download code add added all projects in the solution, projects
    are created within the github folder.

---
+++ MeteredRateFareCalculation/TestFareCalculation/TravelTest.cs
@@ -0,0 +1,66 @@
+﻿using FareCalculationLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestFareCalculation
+{
+
+
+    /// <summary>
+    ///This is a test class for TravelTest and is intended
+    ///to contain all TravelTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class TravelTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        /// <summary>
+        ///A test for CalculateFare
+        ///</summary>
+        [TestMethod()]
+        public void CalculateFareTest()
+        {
+            DateTime dtStart = DateTime.Parse("2010-10-08 5:30 pm");
+            Travel target = new Travel(5, 2, dtStart.Date, dtStart.ToString("HH:mm"));
+            double expected = 9.25F;
+            double actual;
+            actual = target.CalculateFare();
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Travel Constructor
+        ///</summary>
+        [TestMethod()]
+        public void TravelConstructorTest()
+        {
+            DateTime dtStart = DateTime.Parse("2010-10-08 5:30 pm");
+            int numberOfMinutes = 2;
+            int numberOfMiles = 2;
+            DateTime dtJourneyStart = dtStart.Date;
+            String strStartTime = dtStart.ToString("HH:mm");
+            Travel target = new Travel(numberOfMinutes, numberOfMiles, dtJourneyStart, strStartTime);
+            Assert.AreEqual(numberOfMinutes, target.NumberOfMilesTraveled);
+            Assert.AreEqual(numberOfMiles, target.NumberOfMilesTraveled);
+            Assert.AreEqual(dtJourneyStart, target.JourneyStartDateTime);
+            Assert.AreEqual(strStartTime, target.JourneyStartTime);
+        }
+    }
+}
